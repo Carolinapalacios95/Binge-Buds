0ba2536fc2f1a5198d1c42089dd018a945cc7af4
unknown <sculptoid11@gmail.com>
2023-06-27 20:19:03 -0400

    Initial Movie Model

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,31 +1,27 @@
 const { Schema } = require('mongoose');
 
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
-const bookSchema = new Schema({
-  authors: [
-    {
-      type: String,
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedMovies` array in User.js
+const movieSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true,
+    }, 
+
+    movieId: {
+        type: String,
+        required: true,
     },
-  ],
-  description: {
-    type: String,
-    required: true,
-  },
-  // saved book id from GoogleBooks
-  movieId: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  link: {
-    type: String,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
+
+    description: {
+        type: String,
+        required: true,
+    },
+
+    image: {
+        type: String,
+    },
+
 });
 
-module.exports = bookSchema;
+module.exports = movieSchema;
