5c13fdfe3378ddde2d0f0dd7fccfaf4af51ea726
Carolina <carolina.palacios1995@gmail.com>
2023-06-29 19:58:05 -0400

    updating resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,67 +5,57 @@ const AuthService = require("../utils/auth");
 
 const resolvers = {
     Query: {
-        //get all users
-        getUser: async () => {
-            return User.find();
+        me: async (parent, args, context) => {
+            if (context.user) {
+              return User.findOne({ _id: context.user._id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          },
         },
-        //get user by id
-        getUserbyId: async ({ userId }) => {
-            return User.findOne({ _id: userId });
-        },
-        //get all movies
-        getMovie: async () => {
-            return Movie.find();
-        },
-        //get movie by id
-        getMoviebyId: async ({ userId }) => {
-            return Movie.findOne({ _id: userId });
-        },
-
         Mutation: {
-            //addUser mutation
-            addUser: async ({ name }) => {
-                return User.create({ name });
-            },
-            //addMovie(To User) mutation
-            addMovie: async ({ userId, Movie }) => {
-                return User.findOneAndUpdate(
-                    { _id: userId },
-                    {
-                        $addToSet: { savedMovies: Movie },
-                    },
-                    {
-                        new: true,
-                        runValidators: true,
-                    }
-                );
-            },
-            //removeMovie(From user) mutation
-            removeMovie: async ({ userId, Movie }) => {
-                return User.findOneAndUpdate(
-                    { _id: userId },
-                    { $pull: { savedMovies: Movie } },
-                    { new: true }
-                );
-            },
-            login: async ({ email, password }) => {
-                const user = await User.findOne({ email });
-
-                if (!user) {
-                    throw new AuthenticationError('No profile with this email found!');
-                }
-
-                const correctPw = await user.isCorrectPassword(password);
-
-                if (!correctPw) {
-                    throw new AuthenticationError('Incorrect password!');
-                }
-
-                const token = signToken(profile);
-                return { token, profile };
-            },
+          addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+          },
+          login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('No user found with this email address');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+          saveMovie: async (parent, { input }, context) => {
+            if (context.user) {
+              return User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedMovies: input } },
+                { new: true}
+              );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          },
+          removeMovie: async (parent, { MovieId }, context) => {
+            if (context.user) {
+              return User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedMovies: { movieId: movieId }} },
+                { new: true}
+              );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          },
         },
-    },
-}
+      };
 
 module.exports = resolvers;
