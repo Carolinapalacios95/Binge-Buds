ae10edb7f97333bc2e0caa9c39c459676bd090bd
Jasmina <122895875+shecodes1222@users.noreply.github.com>
2023-06-29 16:49:54 -0400

    updated code on API.js file added movie api from rapid api database

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -29,20 +29,20 @@ export const loginUser = (userData) => {
 };
 
 // save book data for a logged in user
-export const saveBook = (bookData, token) => {
+export const savedMovies = (movieData, token) => {
   return fetch('/api/users', {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
       authorization: `Bearer ${token}`,
     },
-    body: JSON.stringify(bookData),
+    body: JSON.stringify(movieData),
   });
 };
 
 // remove saved book data for a logged in user
-export const deleteBook = (bookId, token) => {
-  return fetch(`/api/users/books/${bookId}`, {
+export const deleteMovie = (movieId, token) => {
+  return fetch(`/api/users/movies/${movieId}`, {
     method: 'DELETE',
     headers: {
       authorization: `Bearer ${token}`,
@@ -50,8 +50,20 @@ export const deleteBook = (bookId, token) => {
   });
 };
 
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
-export const searchGoogleBooks = (query) => {
-  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+const url = 'https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4aba6eaba8msh77fce36ea9710a3p122391jsn96c80b6aff8e',
+		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+	}
 };
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+};
+
