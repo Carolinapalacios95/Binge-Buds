8cc91b221098c389a191bfa84f7a920343d2598d
unknown <sculptoid11@gmail.com>
2023-06-29 19:13:44 -0400

    Committing to pull

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,11 +3,11 @@ const { User } = require("../models");
 const resolvers = {
     Query: {
         //get all users
-        User: async () => {
+        getUser: async () => {
             return User.find();
         },
         //get user by id
-        User: async (parent, { userId }) => {
+        getUserbyId: async (parent, { userId }) => {
             return User.findOne({ _id: userId });
         },
         //get all movies
@@ -22,31 +22,47 @@ const resolvers = {
         Mutation: {
             //addUser mutation
             addUser: async (parent, { name }) => {
-              return User.create({ name });
+                return User.create({ name });
             },
             //addMovie(To User) mutation
             addMovie: async (parent, { userId, Movie }) => {
-              return User.findOneAndUpdate(
-                { _id: userId },
-                {
-                  $addToSet: { savedMovies: Movie },
-                },
-                {
-                  new: true,
-                  runValidators: true,
-                }
-              );
+                return User.findOneAndUpdate(
+                    { _id: userId },
+                    {
+                        $addToSet: { savedMovies: Movie },
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                );
             },
             //removeMovie(From user) mutation
             removeMovie: async (parent, { userId, Movie }) => {
-              return Profile.findOneAndUpdate(
-                { _id: profileId },
-                { $pull: { savedMovies: Movie } },
-                { new: true }
-              );
+                return Profile.findOneAndUpdate(
+                    { _id: profileId },
+                    { $pull: { savedMovies: Movie } },
+                    { new: true }
+                );
             },
-          },
+            login: async (parent, { email, password }) => {
+                const user = await User.findOne({ email });
+
+                if (!user) {
+                    throw new AuthenticationError('No profile with this email found!');
+                }
+
+                const correctPw = await user.isCorrectPassword(password);
+
+                if (!correctPw) {
+                    throw new AuthenticationError('Incorrect password!');
+                }
+
+                const token = signToken(profile);
+                return { token, profile };
+            },
         },
-    }
+    },
+}
 
 
