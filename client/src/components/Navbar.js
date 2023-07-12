4a99ed10c4c7fa10082600959dfb82ca51ada337
Jasmina <122895875+shecodes1222@users.noreply.github.com>
2023-07-11 21:00:26 -0400

    updated styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -22,7 +22,7 @@ const AppNavbar = () => {
           <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
             <Nav className='ml-auto d-flex'>
               <Nav.Link as={Link} to='/'>
-                Search For Movies
+                Search For Movies 
               </Nav.Link>
               {/* if user is logged in show saved movies and logout */}
               {Auth.loggedIn() ? (
