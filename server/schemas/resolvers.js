5466dc9fd7b96c16448cb52b8d65b3f5531dfcac
Carolina <carolina.palacios1995@gmail.com>
2023-07-11 20:59:55 -0400

    debugged remove movie

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,72 +3,71 @@ const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require("../utils/auth");
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 require('dotenv').config();
-const apiKey= process.env.API_KEY;
+const apiKey = process.env.API_KEY;
 console.log("API KEY", apiKey);
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if (context.user) {
-              return User.findOne({ _id: context.user._id });
-            }
-            throw new AuthenticationError('You need to be logged in!');
-          },
-          searchMovie: async (parent, { query }) => {
-            const params = query ? { query } : {};
-            const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`);
-            const movies = await response.json();
-            console.log("MOVIES", movies);
-            return movies;
-          }
-        },
-        Mutation: {
-          addUser: async (parent, { username, email, password }) => {
-            const user = await User.create({ username, email, password });
-            const token = signToken(user);
-            return { token, user };
-          },
-          login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-      
-            if (!user) {
-              throw new AuthenticationError('No user found with this email address');
-            }
-      
-            const correctPw = await user.isCorrectPassword(password);
-      
-            if (!correctPw) {
-              throw new AuthenticationError('Incorrect credentials');
-            }
-      
-            const token = signToken(user);
-      
-            return { token, user };
-          },
-          saveMovie: async (parent, { input }, context) => {
-            let updatedUser;
-            if (context.user) {
-              updatedUser = await User.findOneAndUpdate(
-                { _id: context.user._id },
-                { $addToSet: { savedMovies: input } },
-                { new: true}
-              );
-            };
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    searchMovie: async (parent, { query }) => {
+      const params = query ? { query } : {};
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`);
+      const movies = await response.json();
+      console.log("MOVIES", movies);
+      return movies;
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-              return updatedUser;
-            }
-          },
-          // removeMovie: async (parent, { movieId }, context) => {
-          //   if (context.user) {
-          //     return User.findOneAndUpdate(
-          //       { _id: context.user._id },
-          //       { $pull: { savedMovies: { movieId } } },
-          //       { new: true }
-          //     );
-          //   }
-          //   throw new AuthenticationError('You need to be logged in!');
-          // },
-        };
-    
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveMovie: async (parent, { input }, context) => {
+      let updatedUser;
+      if (context.user) {
+        updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedMovies: input } },
+          { new: true }
+        );
+      }
+
+      return updatedUser;
+    },
+    removeMovie: async (parent, { movieId }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedMovies: { movieId } } },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
 
 module.exports = resolvers;
